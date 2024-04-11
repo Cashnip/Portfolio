164aa18b9ccdff3421565f3375c6b838aa4b3019
Cashnip <lucas.sung6@gmail.com>
2024-04-11 11:03:26 -0300

    Added vercel analytics

--- SungCodes/src/App.tsx
+++ SungCodes/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { SpeedInsights } from '@vercel/speed-insights/react'
+import { Analytics } from '@vercel/analytics/react'
 import './App.css'
 
 function App() {
@@ -30,6 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <SpeedInsights />
+      <Analytics />
     </>
   )
 }
